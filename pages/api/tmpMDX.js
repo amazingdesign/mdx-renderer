c6d2ad10f516dc23e97ab90a64c9689d56960693
Mateusz Choma <chomamateusz@gmail.com>
2019-09-13 15:25:37 +0200

    Method not allowed in api tmpMDX

--- pages/api/tmpMDX.js
+++ pages/api/tmpMDX.js
@@ -1,7 +1,16 @@
 import db from '../../src/db'
 
 export default (req, res) => {
-  const { content } = req.body
+  const { method, content } = req.body
+
+  if (method !== 'POST') {
+    res.status(500).json({
+      error: 'Method Not Allowed',
+      code: 405,
+    })
+
+    return
+  }
 
   db.insert(
     { content },
@@ -9,10 +18,10 @@ export default (req, res) => {
       if (err) res.status(500).send(err)
 
       setTimeout(
-      () => db.remove({ _id: newDoc._id }),
+        () => db.remove({ _id: newDoc._id }),
         5 * 60 * 1000
       )
-      
+
       res.json(newDoc)
     }
   )
