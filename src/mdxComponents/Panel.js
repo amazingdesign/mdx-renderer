decbac4abf4ad12859a09da190cc9ccdd3a8e47d
Amazing Design <github@amazingdesign.eu>
2019-10-10 22:22:12 +0200

    Simpler === better. Do not nest MDX runtime component. Simply display children.

--- src/mdxComponents/Panel.js
+++ src/mdxComponents/Panel.js
@@ -1,17 +1,10 @@
 import React from 'react'
 
-import ChildrenUtils from 'react-children-utilities'
-import MDX from '@mdx-js/runtime'
 import Tooltip from '@material-ui/core/Tooltip'
 
 import BackgroundFieldWrapper from './BackgroundFieldWrapper'
 
 const Panel = ({ title, children, tooltip }) => {
-  const mdxComponentsOverwrite = {
-    // its needed to delete unnecessary padding on wrapper
-    wrapper: (props => <div {...props} />)
-  }
-
   const titleElement = (
     title ?
       <h3 style={{ textAlign: 'center' }}>
@@ -28,21 +21,12 @@ const Panel = ({ title, children, tooltip }) => {
       (props) => <React.Fragment {...props} />
   )
 
-  console.log(ChildrenUtils.groupByType(children, ['text']))
-
   return (
     <BackgroundFieldWrapper>
       <TooltipComponent>
         {titleElement}
       </TooltipComponent>
-      {
-        React.Children.map(
-          children,
-          child => (
-            <MDX components={mdxComponentsOverwrite}>{ChildrenUtils.onlyText(child)}</MDX>
-          )
-        )
-      }
+      {children}
     </BackgroundFieldWrapper>
   )
 }
