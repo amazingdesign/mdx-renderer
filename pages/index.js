a3e3ccd64ab71df68f438b03442b799ee278e1d5
Mateusz <chomamateusz@gmail.com>
2020-03-26 08:42:44 +0100

    Added DISPLAY_WAITING_FOR_RESPONSIVE to .env

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ContentDisplayer from '../src/components/ContentDisplayer'
 
+import { getConfig } from '@bit/amazingdesign.utils.config'
+
+const displayWaitingForResponsive = getConfig('DISPLAY_WAITING_FOR_RESPONSIVE') === 'false' ? false : Boolean(getConfig('DISPLAY_WAITING_FOR_RESPONSIVE'))
+
 const UrlPage = (props) => (
   <ContentDisplayer
-    displayWaitingForResponsive={true}
+    displayWaitingForResponsive={displayWaitingForResponsive}
     {...props}
   />
 )
